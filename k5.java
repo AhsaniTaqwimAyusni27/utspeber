ea33d619f95b98b19a8939debccce374517a677d
AhsaniTaqwimAyusni27 <127710945+AhsaniTaqwimAyusni27@users.noreply.github.com>
2023-05-22 11:02:43 +0700

    Add files via upload

---
+++ k5.java
@@ -0,0 +1,14 @@
+package id.ac.utsappmob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class k5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d5);
+    }
+}
